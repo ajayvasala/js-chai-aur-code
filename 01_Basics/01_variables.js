166c0917330d46c81e79f2489994549b89c2d7f6
Ajay Vasala <61384214+ajayvasala@users.noreply.github.com>
2024-02-14 04:16:03 +0000

    Vaiables

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -2,3 +2,18 @@ const accountID = 144553;
 let accountEmail = "ajay@yahoo.com";
 var accountPassword = "12345";
 accountCity = "Mumbai";
+let accountState;
+
+// accountID = 2;
+
+accountEmail = "aj@ajay.com";
+accountPassword = "212121";
+accountCity = "Pune";
+
+console.log(accountID);
+
+/* prefer not to use var 
+    because of issue in block scope and functional scope 
+*/
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
