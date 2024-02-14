a3ece483e1e47de6f399ba1deee66148bfca4fd6
Ajay Vasala <61384214+ajayvasala@users.noreply.github.com>
2024-02-14 04:15:42 +0000

    JS Datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"  //treat all JS code as  newer version
+
+//alert (3 + 3);   //can not use in node js, we can use this in console and bind with html file in browser
+
+console.log(3 
+    + 
+    3);     // code readability is should be high
+
+let name = "Ajay"
+let age = 32
+let isLoggedIn = true
+let state;
+let temperature = null
+
+//Primitive Data types
+// string => ""
+// number => to the range of 2 to power of 53
+// bigint
+// boolean => true/false
+// undefined => varibale declared but not assigned any value to it
+// null => standalone value
+// symbol => to fing uniqueness
+
+
+//Reference Data types
+// Object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
