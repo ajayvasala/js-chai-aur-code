8922e26729db7b63c725b07e31c87abf3b66d155
Ajay Vasala <61384214+ajayvasala@users.noreply.github.com>
2024-02-15 03:40:04 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1); // dont attempt to copmare 2 different data types - it will give inaccurate result
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log("2" === 2); //strict comparison
+
+
