ea321c788e59d6a2d6efb91da91723cef512d2fc
Ajay Vasala <61384214+ajayvasala@users.noreply.github.com>
2024-04-03 14:32:36 +0000

    updated

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -10,7 +10,7 @@ let name = "Ajay"
 let age = 32
 let isLoggedIn = true
 let state;
-let temperature = null
+let temperature = null //(ex: if i want to know temperature from server and for some reason server unable to find the temperature and returns 0 then in this case 0 is also temperature. So, we can assign null data type in this case)
 
 //Primitive Data types
 // string => ""
@@ -19,7 +19,7 @@ let temperature = null
 // boolean => true/false
 // undefined => varibale declared but not assigned any value to it
 // null => standalone value
-// symbol => to fing uniqueness
+// symbol => to find uniqueness
 
 
 //Reference Data types
