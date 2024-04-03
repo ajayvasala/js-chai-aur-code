91378ee6e49fee1303272eda2de75f9de7d0ed2f
Ajay Vasala <61384214+ajayvasala@users.noreply.github.com>
2024-04-03 14:40:04 +0000

    updated comments

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -5,11 +5,11 @@ let xyz = undefined;
 let boolean = true;
 
 
-console.log(typeof score);  // number
-console.log(typeof scoreString); // string
-console.log(typeof abc);  //object
-console.log(typeof xyz);  //undefined
-console.log(typeof boolean);  //boolean
+console.log(8,typeof score);  // number
+console.log(9,typeof scoreString); // string
+console.log(10,typeof abc);  //object
+console.log(11,typeof xyz);  //undefined
+console.log(12,typeof boolean);  //boolean
 
 
 let valueInNumber = Number(score) // We can use this method to convert variable into number
@@ -19,19 +19,19 @@ let valueInNumber4 = Number(xyz)
 let valueInNumber5 = Number(boolean)
 
 
-console.log(typeof valueInNumber);  //number
-console.log(typeof valueInNumber2); //number
-console.log(typeof valueInNumber3); //number
-console.log(typeof valueInNumber4); //number
-console.log(typeof valueInNumber5); //number
+console.log(22,typeof valueInNumber);  //number
+console.log(23,typeof valueInNumber2); //number
+console.log(24,typeof valueInNumber3); //number
+console.log(25,typeof valueInNumber4); //number
+console.log(26,typeof valueInNumber5); //number
 
 
 
-console.log(valueInNumber);
-console.log(valueInNumber2); // NaN
-console.log(valueInNumber3); // 0
-console.log(valueInNumber4); // NaN
-console.log(valueInNumber5); // 1
+console.log(30,valueInNumber);
+console.log(31,valueInNumber2); // NaN
+console.log(32,valueInNumber3); // 0
+console.log(33,valueInNumber4); // NaN
+console.log(34,valueInNumber5); // 1
 
 
 // "33" => 33
@@ -42,7 +42,7 @@ console.log(valueInNumber5); // 1
 let isLoggedIn = "ajay"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+console.log(45,booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -53,15 +53,15 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+console.log(56,typeof stringNumber);
+console.log(57,stringNumber);
 
 
 //***************************** Operations ***********************************
 
 let value = 3
 let negValue = -value
-console.log(negValue);
+console.log(64,negValue);
 
 // console.log(2+2);
 // console.log(2-2);
@@ -73,15 +73,15 @@ let str1 = "hello"
 let str2 = " ajay"
 
 let str3 = str1 + str2
-console.log(str3);
+console.log(76,str3);
 
-console.log("1" + 2); 
-console.log(1 + "2");
-console.log("1" + 2 + 2);  //if we put string first in arithmatic operations then remaining numbers will count as string only - 122
-console.log(1 + 2 + "2"); //first numbers will add then string will add into that number - 32
-console.log(1 + "4" + 2); // - 142
+console.log(78, "1" + 2); 
+console.log(79, 1 + "2");
+console.log(80, "1" + 2 + 2);  //if we put string first in arithmatic operations then remaining numbers will count as string only - 122
+console.log(81, 1 + 2 + "2"); //first numbers will add then string will add into that number - 32
+console.log(82, 1 + "4" + 2); // - 142
 
-console.log( (3 + 4) * 5 - 1);
+console.log(84,  (3 + 4) * 5 - 1);
 
 
 let gameCounter = 100
@@ -89,6 +89,6 @@ let gameCounter = 100
                         // increments and returns the value before incrementing.
 gameCounter++;          //postfix - If used prefix, with operator before operand (for example, ++x), the increment operator increments  and returns the value after incrementing.
 
-console.log(gameCounter);
+console.log(102, gameCounter);
 
 
